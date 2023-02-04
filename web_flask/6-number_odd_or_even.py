567a0a2f7e1ed0dd0a7124cb91a94ef24d932b12
Don-001 <claudiodex009@gmail.com>
2023-02-05 01:22:28 +0300

    Add a 6-munber.html in templates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,13 +40,13 @@ def is_n_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ display a HTML page only if n is an integer """
-    return render_template('5-number.html', value=n)
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     """ display a HTML page only if n is an integer """
-    return render_template('6-number_odd_or_even.html', value=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
